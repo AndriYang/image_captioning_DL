755b6900fb13bf0cc94b86aeed0bebf88abf4021
nixster <iamnixster@gmail.com>
2020-04-24 12:30:01 +0000

    updated sample.py for dynamicness

--- tutorials/03-advanced/image_captioning/sample.py
+++ tutorials/03-advanced/image_captioning/sample.py
@@ -4,6 +4,8 @@ import numpy as np
 import argparse
 import pickle 
 import os
+from os import listdir
+from os.path import isfile, join
 from torchvision import transforms 
 from build_vocab import Vocabulary
 from model import EncoderCNN, DecoderRNN
@@ -70,8 +72,14 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--image', type=str, required=True, help='input image for generating caption')
-    parser.add_argument('--encoder_path', type=str, default='models/encoder-2-3000.ckpt', help='path for trained encoder')
-    parser.add_argument('--decoder_path', type=str, default='models/decoder-2-3000.ckpt', help='path for trained decoder')
+    
+    # Find the latest encoder and decoder
+    model_root = 'models'
+    files = [f for f in listdir(model_root) if isfile(join(model_root, f))]
+    latest = int(len(files)/6)
+    
+    parser.add_argument('--encoder_path', type=str, default=f'models/encoder-{latest}-3000.ckpt', help='path for trained encoder')
+    parser.add_argument('--decoder_path', type=str, default=f'models/decoder-{latest}-3000.ckpt', help='path for trained decoder')
     parser.add_argument('--vocab_path', type=str, default='data/vocab.pkl', help='path for vocabulary wrapper')
     
     # Model parameters (should be same as paramters in train.py)
