7c889560c653bc1fcdd35e83f8ccef6ffc532627
grand3110 <bhvymtl@outlook.com>
2020-04-26 14:35:00 +0000

    removed unnecessary files

--- tutorials/03-advanced/image_captioning/download_glove6B.sh
+++ tutorials/03-advanced/image_captioning/download_glove6B.sh
@@ -1,5 +1,6 @@
 mkdir glove_data
 wget http://nlp.stanford.edu/data/wordvecs/glove.6B.zip -P ./glove_data/
 
-unzip ./glove_data/glove.6B.zip
-rm ./glove_data/glove.6B.zip
+cd glove_data
+unzip glove.6B.zip
+#rm ./glove_data/glove.6B.zip
